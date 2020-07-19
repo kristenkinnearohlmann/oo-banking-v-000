7e624f5454ffa2e6f75f3db8efc06a3ea840bbc6
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-19 22:53:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,8 +23,8 @@ class Transfer
   def execute_transaction
     if @status == "complete"
 
-    # elsif @sender.status = "closed"
-    #   "Transaction rejected. Please check your account balance."
+    elsif (@sender.balance - @amount <0 || @sender.status = "closed")
+      "Transaction rejected. Please check your account balance."
     else
       @sender.balance -= @amount
       @receiver.deposit(@amount)
